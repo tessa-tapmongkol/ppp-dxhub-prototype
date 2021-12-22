a04c94da2489ba5a57b27ee0af2d22d8821940a7
Tessa Tapmongkol <ttapmong@calpoly.edu>
2021-12-22 00:36:03 -0800

    Added modal

--- src/Pages/ComparePage/ComparePage.js
+++ src/Pages/ComparePage/ComparePage.js
@@ -1,49 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Card from "../../Components/Card/Card";
-import axios from "axios";
 import { styleSubcategory } from "../../constants/stringFunctions";
-import { Oval } from 'react-loading-icons'
+import ReactModal from "react-modal";
+import { sort_subcategory } from "../../constants/subcategoryitems";
+import { AiOutlineClose } from 'react-icons/ai';
+import { data } from "../../constants/subcategoryitems";
+import SortCard from "../../Components/SortCard/SortCard";
 
 function ComparePage(props) {
-    const [data, setData] = React.useState([]);
     const [similar, setSimilar] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
-
-    const url = process.env.REACT_APP_API_URL + props.query.toLowerCase() + "&category=" + props.category;
-
-    async function fetchAll() {
-        try {
-            return await axios.get(url, {
-                headers: {
-                    'x-api-key': process.env.REACT_APP_API_KEY,
-                    'Content-Type': 'application/json'
-                }
-            });
-        }
-        catch (error) {
-            console.log(error);
-            return false;
-        }
-    }
-    React.useEffect(() => {
-        fetchAll().then(result => {
-            if (result && result !== undefined) {
-                if (props.query === "Beef") parseTargetItems(result.data.stores.target.California["5659"]);
-                else parseTargetItems(result.data.stores.target);
-                parseWalmartItems(result.data.stores.walmart);
-                parseNoonItems(result.data.stores.noon);
-                parseUbuyItems(result.data.stores.ubuy);
-                setLoading(false);
-            }
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    function removeProd() {
-        setData(data.filter((prod, i) => { return !similar.includes(i); }));
-        setSimilar([]);
-    }
+    const [showModal, setShowModal] = React.useState(false);
 
     function getCards(prod, index) {
         return (
@@ -63,79 +30,35 @@ function ComparePage(props) {
         );
     }
 
-    function parseTargetItems(prods) {
-        const targetItems = [];
-        for (const key in prods) {
-            const prod = prods[key];
-            targetItems.push({
-                store: "Target",
-                brand: prod?.info?.primary_brand?.name,
-                name: prod?.info?.product_description?.title,
-                image: prod?.info?.enrichment?.images?.primary_image_url,
-                price: prod?.pricing?.current_retail,
-                desc: prod?.info?.product_description?.soft_bullets,
-                link: prod?.info?.enrichment?.buy_url
-            })
-        }
-        setData(oldData => [...oldData, ...targetItems]);
+    function removeProd() {
+        data.filter((prod, i) => { return !similar.includes(i); });
+        setSimilar([]);
     }
 
-    function parseWalmartItems(prods) {
-        const walmartItems = [];
-        for (const key in prods) {
-            const prod = prods[key];
-            const prodUrl = "https://www.walmart.com/" + prod?.info?.productUrl;
-            walmartItems.push({
-                store: "Walmart",
-                brand: prod?.info?.name,
-                name: prod?.info?.name,
-                image: prod?.info?.image?.thumbnail,
-                price: prod?.pricing?.displayPrice,
-                desc: [""],
-                link: prodUrl
-            })
-        }
-        setData(oldData => [...oldData, ...walmartItems]);
-    }
-
-    function parseNoonItems(prods) {
-        const noonItems = [];
-        for (const key in prods) {
-            const prod = prods[key];
-            const prodUrl = "https://www.noon.com/uae-en/" + prod?.url + "/p?o=";
-            const image = "https://z.nooncdn.com/products/tr:n-t_80/" + prod?.images?.image_keys[0] + ".jpg";
-            noonItems.push({
-                store: "Noon",
-                brand: prod?.brand,
-                name: prod?.name,
-                image: image,
-                price: prod?.pricing?.price,
-                desc: [""],
-                link: prodUrl
-            })
-        }
-        setData(oldData => [...oldData, ...noonItems]);
-    }
-
-    function parseUbuyItems(prods) {
-        const ubuyItems = [];
-        for (const key in prods) {
-            const prod = prods[key];
-            ubuyItems.push({
-                store: "Ubuy",
-                brand: "",
-                name: prod?.specifications,
-                image: prod?.image,
-                price: prod?.price,
-                desc: [prod?.description],
-                link: ""
-            })
-        }
-        setData(oldData => [...oldData, ...ubuyItems]);
+    function getSimilarString() {
+        var result = "";
+        const similarProd = data.filter((prod, i) => { return similar.includes(i); })
+        similarProd.forEach(product => result += "\"" + product.name + "\" ")
+        return result;
     }
 
     return (
         <body>
+            <ReactModal isOpen={showModal} className="sortModal">
+                <div>
+                    <div style={{ textAlign: "right" }}>
+                        <AiOutlineClose onClick={() => setShowModal(false)} className="exitModalButton" />
+                    </div>
+                    <h1>{"Add " + getSimilarString() + " to"}</h1>
+                    <div className="sortNameCards">
+                        {sort_subcategory.map(sub => {
+                            return (
+                                <SortCard sub={sub} />
+                            )
+                        })}
+                    </div>
+                </div>
+            </ReactModal>
             <Link to={"/subcategories/" + props.category}><button className="subcategoryButton">Back to list of subcategories</button></Link>
             <Link to="/"><button className="searchButton">Back to search</button></Link>
             <br />
@@ -143,13 +66,16 @@ function ComparePage(props) {
             <h1>{styleSubcategory(props.query)}</h1>
             <div className="compareHeader">
                 <h3>Select items that are similar</h3>
-                <button className="compareButton" onClick={() => { removeProd(); }}>{"These " + similar.length + " items are similar"}</button>
+                <div>
+                    <button className="differentCategoryButton" onClick={() => setShowModal(true)}>Add to different category</button>
+                    <button className="compareButton" onClick={() => { removeProd(); }}>{"These " + similar.length + " items are similar"}</button>
+                </div>
             </div>
             <div className="comparePage">
                 <div className="compareCards">
-                    {!loading ? data.map((targetProd, index) => { return getCards(targetProd, index); }) : <Oval fill="transparent" stroke="#06bcee" strokeOpacity={1} strokeWidth={2} />}
+                    {data.map((prod, index) => { return getCards(prod, index); })}
                 </div>
-            </div>
+            </div>  
         </body>
     )
 }
--- src/constants/subcategoryitems.js
+++ src/constants/subcategoryitems.js
@@ -1,3 +1,36 @@
+export const data = [
+    {
+        store: "Walmart",
+        brand: "Imperial",
+        name: "Oil spread, 1 lb, 4 Sticks",
+        image: "https://i5.wal.co/asr/d6155979-2676-4381-97bf-990ce9db98b2_1.21dab52d58f71361ebb6b67602d1e11f.png?odnBg=ffffff&odnHeight=612&odnWidth=612",
+        price: "1.00",
+        desc: ["Vegetable Oil Spread, 53%, Box 16 OZ Good for baking! 0 g trans fat per serving. See nutrition information for fat and saturated fat content. Crowned with quality. Per 1 Tbsp: 70 calories; 2.5 g sat fat (13% DV); 105 mg sodium (4% DV); 0 g sugars. Gluten free. No partially hydrogenated oils. Best if used by date on package. For best quality do not freeze."],
+        link: "https://www.walmart.com/ip/Imperial-West-Qtrs/435517604",
+        key: "1"
+    },
+    {
+        store: "Walmart",
+        brand: "I Can't Believe It's Not Butter",
+        name: "Original Vegetable Oil Spread, 15 oz Tub",
+        image: "https://i5.walmartimages.com/asr/c9d05f60-4ab7-4e96-8ebc-578766073d5c.dcff14eac084025f2badd29f77b4f05c.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+        price: "1.00",
+        desc: ["I Can’t Believe It’s Not Butter! Original spread is made with good fats from plant-based oils like omega-3 ALA.* It meets the nutritional requirements of the American Heart Association Heart-Check certification program. ** Made with simple ingredients, with a delicious blend of oils, purified water, and a pinch of salt."],
+        link: "https://www.walmart.com/ip/Imperial-West-Qtrs/435517604",
+        key: "2"
+    },
+    {
+        store: "Walmart",
+        brand: "Pompeian",
+        name: "Smooth Extra Virgin Olive Oil, 16 fl oz",
+        image: "https://i5.walmartimages.com/asr/2dd28180-163b-4448-9a35-c9e631897dc3.690d387329c3dab3168eaa982d4444a4.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+        price: "5.18",
+        desc: ["Expertly crafted by The Olive Oil People, this imported, first cold pressed extra virgin olive oil is authentically crafted by Pompeian’s family of olive farmers and olive oil craftsmen. From start to finish, you can taste the difference in Pompeian olive oils because our olive farmers and olive oil craftsmen have carefully nurtured the world’s best olives from grove to table, creating the best-tasting, highest-quality olive oil. Rooted in authenticity, our olive oils are crafted to help you eat and live well every day. Pompeian Smooth Extra Virgin Olive Oil is perfect for sautéing and stir-frying."],
+        link: "https://www.walmart.com/ip/Imperial-West-Qtrs/435517604",
+        key: "3"
+    }
+]
+
 export const oils_fats = [
     "margarine",
     "butter",
@@ -70,3 +103,14 @@ export const category_type = {
     VEGETABLES: "vegetables",
     SMALL_ELECTRICITY: "small_electric_household_appliances"
 }
+
+export const sort_subcategory = [
+    "Category",
+    "Category",
+    "Category",
+    "Category",
+    "Category",
+    "Category",
+    "Category",
+    "Category"
+]
